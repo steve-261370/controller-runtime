3dbf5a65f532309f0413a8664881eb4060a2e21e
Steve Nguyen <stevehuynguyen@google.com>
2021-07-15 03:03:49 +0000

    Add an example RBAC test
    This example RBAC test does the following:
    
     - Creates a new user on the cluster and shows that it doesn't have
     cluster-admin rights and can't read pods
     - Adds the necessary RBAC to grant the user access to read pods
     - Verifies that the user can now read pods

--- pkg/internal/testing/controlplane/plane_test.go
+++ pkg/internal/testing/controlplane/plane_test.go
@@ -22,6 +22,8 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	kauthn "k8s.io/api/authorization/v1"
+	rbac "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	. "sigs.k8s.io/controller-runtime/pkg/internal/testing/controlplane"
@@ -97,6 +99,102 @@ var _ = Describe("Control Plane", func() {
 		// legacy user flow, but we should be explicit
 
 		Describe("adding users", func() {
+			It("should add new users with different RBAC", func() {
+
+				By("adding a test user")
+				user, err := plane.AddUser(User{
+					Name:   "test-user",
+					Groups: []string{"pod-reader-group"},
+				}, nil)
+				Expect(err).NotTo(HaveOccurred(), "should be able to add a user")
+
+				By("grabbing the REST config for the new user and testing that it has no permissions")
+				cfg := user.Config()
+				cl, err := client.New(cfg, client.Options{})
+				Expect(err).NotTo(HaveOccurred(), "should be able to create a client")
+
+				sar := &kauthn.SelfSubjectAccessReview{
+					Spec: kauthn.SelfSubjectAccessReviewSpec{
+						ResourceAttributes: &kauthn.ResourceAttributes{
+							Verb:     "*",
+							Group:    "*",
+							Version:  "*",
+							Resource: "*",
+						},
+					},
+				}
+				Expect(cl.Create(context.Background(), sar)).Should(Succeed(), "should be able to make a Self-SAR")
+				Expect(sar.Status.Allowed).To(BeFalse(), "unconfigured users should not be able to do everything")
+
+				readPod := &kauthn.SelfSubjectAccessReview{
+					Spec: kauthn.SelfSubjectAccessReviewSpec{
+						ResourceAttributes: &kauthn.ResourceAttributes{
+							Verb:     "get",
+							Group:    "",
+							Version:  "v1",
+							Resource: "pods",
+						},
+					},
+				}
+				Expect(cl.Create(context.Background(), readPod)).Should(Succeed(), "should be able to make a Self-SAR")
+				Expect(readPod.Status.Allowed).To(BeFalse(), "unconfigured users should not be able to read pods")
+
+				By("adding permissions to the new user and verifying it works")
+				rootCFG, err := plane.RESTClientConfig()
+				Expect(err).NotTo(HaveOccurred(), "should be able to grab the legacy REST config")
+				rootClient, err := client.New(rootCFG, client.Options{})
+				Expect(err).NotTo(HaveOccurred(), "should be able to create a client")
+
+				podReaderRole := &rbac.ClusterRole{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod-reader",
+						Namespace: metav1.NamespaceSystem,
+					},
+					Rules: []rbac.PolicyRule{
+						{
+							Verbs:     []string{"get"},
+							APIGroups: []string{""},
+							Resources: []string{"pods"},
+						},
+					},
+				}
+
+				binding := &rbac.ClusterRoleBinding{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "pod-reader-binding",
+					},
+					Subjects: []rbac.Subject{
+						{
+							Kind: "Group",
+							Name: "pod-reader-group",
+						},
+					},
+					RoleRef: rbac.RoleRef{
+						APIGroup: "rbac.authorization.k8s.io",
+						Name:     "pod-reader",
+						Kind:     "ClusterRole",
+					},
+				}
+
+				Expect(rootClient.Create(context.Background(), podReaderRole)).Should(Succeed(), "should be able to create a pod-reader role")
+				Expect(rootClient.Create(context.Background(), binding)).Should(Succeed(), "should be able to create pod-reader role binding")
+
+				readPod2 := &kauthn.SelfSubjectAccessReview{
+					Spec: kauthn.SelfSubjectAccessReviewSpec{
+						ResourceAttributes: &kauthn.ResourceAttributes{
+							Verb:     "get",
+							Group:    "",
+							Version:  "v1",
+							Resource: "pods",
+						},
+					},
+				}
+
+				Expect(cl.Create(context.Background(), readPod2)).Should(Succeed(), "should be able to make a Self-SAR")
+				Expect(readPod2.Status.Allowed).To(BeTrue(), "now the new user can read pods!")
+
+			})
+
 			PIt("should be able to provision new users that have a corresponding REST config and & kubectl", func() {
 
 			})
